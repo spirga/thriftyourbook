c9f0a26ce2a7da69cba3b40c37a87f92e715fce6
Loreta Rutkovska <loreta_rutkovska@inbox.lv>
2022-06-19 22:04:50 +0300

    Migartions: genres books booksgenres bookeditions

---
+++ database/migrations/2022_06_19_185516_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 100);
+            $table->string('author', 50);
+            $table->string('description', 2000);
+            $table->integer('year');
+            $table->string('language', 3);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2022_06_19_185543_create_bookeditions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookeditions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('book_id')->constrained();
+            $table->string('imageurl');
+            $table->string('publisher', 50);
+            $table->integer('editionyear');
+            $table->integer('pages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookeditions');
+    }
+};
