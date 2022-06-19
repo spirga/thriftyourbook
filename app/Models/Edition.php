144e63ae1ed907d67348224d766aaf30a699c46b
Loreta Rutkovska <loreta_rutkovska@inbox.lv>
2022-06-19 22:22:45 +0300

    Created Models

---
+++ app/Models/Edition.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Book;
+
+class Edition extends Model
+{
+    use HasFactory;
+    public function book()
+{ // FK relationship
+return $this->belongsTo(Book::class);
+}
+}
