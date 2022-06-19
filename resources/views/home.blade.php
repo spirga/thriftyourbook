a83d0eb7bb2c0c05a9aa6ac08b1dd1369fc86124
Renāte Spirga <spirgarenate@gmail.com>
2022-06-19 05:14:59 +0300

    design changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-	<x-slot name="header" class="bg-gray-100">
+	<x-slot name="header">
 		<div class="pt-10 justify-center w-screen items-center flex flex-col">
 			<span class="font-elsie text-title">THRIFT YOUR BOOK</span>
 			<span class="font-fasthand text-subtitle mt-4 mb-4">- where books find their people -</span>
@@ -14,15 +14,17 @@
 					</svg>
 				</div> -->
 			<div class="flex mr-25 "><input type="search" id="default-search"
-					class="block px-4 pl-10 w-full text-sm text-gray-900 bg-darker rounded-lg border border-gray-200 focus:ring-0 focus:border-gray-100 focus:outline-0"
+					class="block px-4 pl-10 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-200 focus:ring-0 focus:border-gray-100 focus:outline-0"
 					placeholder="Search books..." required></div>
 			<div class="flex justify-center items-center flex-col ml-25"><button type="submit" class=" mt-1 text-custom absolute right-2.5 bottom-2.5 bg-custom hover:bg-custom-lighter focus:ring-4
 				focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-4 py-2 text-white ">Search</button>
 			</div>
 		</form>
 	</x-slot>
+	<div class="flex justify-center">
+		<hr class="w-80p ">
+	</div>
 
-	<hr class="mt-4">
 
 	<div class="grid grid-cols-2">
 		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -37,10 +39,10 @@
 				{{ __('Available books') }}
 			</h2>
 			<div>
-				<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-					<div class="p-6 bg-white border-b border-gray-200">
-						seit jaliek gramatas
-					</div>
+
+				<div class="p-6 bg-white">
+					seit jaliek gramatas
+
 				</div>
 			</div>
 		</div>
