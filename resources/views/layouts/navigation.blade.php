ed769c65a5a01fd384f1d1544efe349494ee8494
Renāte Spirga <spirgarenate@gmail.com>
2022-06-20 21:30:52 +0300

    added design changes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-gray-100 border-b border-gray-100 fixed z-50 top-0 w-full">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 fixed z-50 top-0 w-full">
 	<!-- Primary Navigation Menu -->
 	<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 		<div class="flex justify-between h-16 w-95p">
@@ -24,7 +24,7 @@
 				</div>
 				<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
 					<x-nav-link :href="route('my-orders')" :active="request()->routeIs('my-orders')">
-						{{ __('Orders') }}
+						{{ __('My Orders') }}
 					</x-nav-link>
 				</div>
 			</div>
@@ -48,9 +48,8 @@
 				<x-dropdown align="right" width="48">
 					<x-slot name="trigger">
 						<button
-							class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-							<div>{{ Auth::user()->name }}</div>
-
+							class="flex items-center text-sm font-medium text-gray-500 hover:text-custom hover:border-gray-300 focus:outline-none focus:text-custom focus:border-gray-300 transition duration-150 ease-in-out">
+							<div><b>{{ Auth::user()->name }}</b></div>
 							<div class="ml-1">
 								<svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 									<path fill-rule="evenodd"
