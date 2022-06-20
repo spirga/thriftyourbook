8e0e7a1041aadedbef20523afdfd3e9a0022ef12
Renāte Spirga <spirgarenate@gmail.com>
2022-06-20 13:59:18 +0300

    created EditionController, BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,29 +4,52 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
-use App\Models\Genre;
-use App\Models\Edition;
 
 class BookController extends Controller
 {
+    public function index()
+    {
+        $books = Book::all();
+        return view('books', compact('books'));
+    }
+    
     public function create()
     {
-        return view('add_book');
+        return view('new_book');
     }
 
     public function store(Request $request) {
-        // $rules = array(
-            
-        // );
-        //$this->validate($request, $rules);
+        $rules = array(
+            'book_title' => 'required|min:1|max:100|string',
+            'book_language' => 'required|alpha|min:2|max:3',
+        );
+        $this->validate($request, $rules);
         $book = new Book;
         $book->book_title = $request->book_title;
         $book->book_description = $request->book_description;
         $book->book_year = $request->book_year;
         $book->book_language = $request->book_language;
-        $book->save();
-        
-        //return
-        
+        $book->save();        
+        //return redirect('country/');
+        return redirect()->back();
+    }
+
+    public function update(Request $request)
+    {
+        $book=Book::find($request->id);
+        $book->book_title = $request->book_title;
+        $book->book_description = $request->book_description;
+        $book->book_year = $request->book_year;
+        $book->book_language = $request->book_language;
+        $book->save();     
+        //return redirect('country/');
+        return redirect()->back();
+    }
+
+    public function destroy($id)
+    {
+        Book::findOrFail($id)->delete();
+        //return redirect('country/');
+        return redirect()->back();
     }
 }
--- app/Http/Controllers/EditionController.php
+++ app/Http/Controllers/EditionController.php
@@ -9,6 +9,13 @@ use App\Models\Edition;
 
 class EditionController extends Controller
 {
+
+    public function index($id)
+    {
+        $editions=Edition::where('book_id','=',$id)->get(); 
+        return view('editions',['book_id'=>$id,'editions'=>$editions]);
+    }
+    
     public function create($id)
     {
         $book = Book::findOrFail($id);
@@ -17,10 +24,13 @@ class EditionController extends Controller
 
     public function store(Request $request)
     {
-        // $rules = array(
-            
-        // );
-        // $this->validate($request, $rules);
+        $rules = array(
+            'publisher' => 'required|string',
+            'edition_year' => 'required|numeric',
+            'pages' => 'required|numeric',
+            'cover_type' => 'required|min:2|max:2',
+        );
+        $this->validate($request, $rules);
         $edition = new Edition;
         $edition->book_id = $request->book_id;
         $edition->image_url = $request->image_url;
@@ -33,4 +43,26 @@ class EditionController extends Controller
         //return redirect('book/edition' . $edition->book_id);
         return redirect()->back();
     }
+
+    public function update(Request $request)
+    {
+        $edition=Edition::find($request->id);
+        $edition->book_id = $request->book_id;
+        $edition->image_url = $request->image_url;
+        $edition->publisher = $request->publisher;
+        $edition->edition_year = $request->edition_year;
+        $edition->pages = $request->pages;
+        $edition->cover_type = $request->cover_type;        
+        $edition->save();
+        //return redirect('country/');
+        return redirect()->back();
+    }
+
+    public function destroy($id)
+    {
+        Edition::findOrFail($id)->delete();
+        //return redirect('country/');
+        return redirect()->back();
+    }
 }
+    
