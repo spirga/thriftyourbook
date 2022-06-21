0387c537eec479bfa5fe705ded0edd488c90bae1
Loreta Rutkovska <loreta_rutkovska@inbox.lv>
2022-06-21 13:11:09 +0300

    fixed listing controller

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 	<x-slot name="header">
 		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-			{{ $listings->$edition_id}}
+			{{ $listing->$edition_id}}
 		</h2>
 	</x-slot>
 
